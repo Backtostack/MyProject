5eaf0775b24fdf49d1f9c6380730a263f03a0224
Backtostack <backtostack@gmail.com>
2018-04-26 17:28:54 +0300

    26.04.2018

--- app/src/main/java/ru/startandroid/myproject/MainActivity.java
+++ app/src/main/java/ru/startandroid/myproject/MainActivity.java
@@ -1,11 +1,17 @@
 package ru.startandroid.myproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 
@@ -13,7 +19,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView txtView;
     Button btnJava;
     Button btnAndroid;
-    ImageView imgView;
+    /*ImageView imgView;*/
+    CheckBox checkBox, checkBox2, checkBox3;
+    SeekBar sbWeight;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,32 +32,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtView = (TextView) findViewById(R.id.textView);
         btnJava = (Button) findViewById(R.id.button);
         btnAndroid = (Button) findViewById(R.id.button2);
-        imgView = (ImageView) findViewById(R.id.imageView);
+        /*imgView = (ImageView) findViewById(R.id.imageView);*/
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        sbWeight = (SeekBar) findViewById(R.id.sbWeight);
+
         //Присваеваем обработчик кнопкам
         btnJava.setOnClickListener(this);
         btnAndroid.setOnClickListener(this);
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // TODO Auto-generated method stub
         // добавляем пункты меню
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem itemaudio = menu.findItem(R.id.saudio);
+        MenuItem itemvideo = menu.findItem(R.id.svideo);
+        if (checkBox.isChecked()) {
+            itemaudio.setVisible(true);
+            itemvideo.setVisible(true);
+        } else {
+            itemaudio.setVisible(false);
+            itemvideo.setVisible(false);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
     @Override
     public void onClick(View v) {
+        Animation animalpha = null;
+        /*Animation animtranslate = null;*/
+        animalpha = AnimationUtils.loadAnimation(this, R.anim.myalpha);
         switch (v.getId()) {
             case R.id.button:
-                txtView.setText(R.string.Java);
-                try {
-                    imgView.setImageDrawable(getResources().getDrawable(R.drawable.ic_airplay_black_48dp));
+                if (checkBox2.isChecked()) {
+                    txtView.setText(R.string.Java);
+                    btnJava.startAnimation(animalpha);
+                } else {
+                    txtView.setText(R.string.Java);
                 }
-                catch (NullPointerException e) {
-                    txtView.setText(R.string.ExpImageView);
+                if (checkBox3.isChecked()) {
+                    Intent intentJ = new Intent(this, ActivityJava.class);
+                    startActivity(intentJ);
+                } else {
+                    txtView.setText(R.string.Java);
                 }
                 break;
             case R.id.button2:
-                txtView.setText(R.string.Android);
-                imgView.setImageDrawable(getResources().getDrawable(R.drawable.ic_equalizer_black_48dp));
+                if (checkBox2.isChecked()) {
+                    txtView.setText(R.string.Android);
+                    btnAndroid.startAnimation(animalpha);
+                } else {
+                    txtView.setText(R.string.Android);
+                }
+                if (checkBox3.isChecked()) {
+                    Intent intentA = new Intent(this, ActivityAndroid.class);
+                    startActivity(intentA);
+                } else {
+                    txtView.setText(R.string.Android);
+                }
+                break;
+            default:
                 break;
         }
     }
